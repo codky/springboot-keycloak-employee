acf730a1986657dc570bc49ce01e6eae2b316ed3
codky <msky0912@gmail.com>
2022-12-26 17:58:01 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootkeycloakexample/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.example.springbootkeycloakexample.controller;
+
+import com.example.springbootkeycloakexample.model.Employee;
+import com.example.springbootkeycloakexample.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Role;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @PostMapping("/admin/employee")
+    @RolesAllowed("admin")
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeRepository.save(employee));
+    }
+
+    @GetMapping("/user/employee")
+    @RolesAllowed({"employee", "admin"})
+    public ResponseEntity<List<Employee>> getEmployee() {
+        return ResponseEntity.ok(employeeRepository.findAll());
+    }
+}
